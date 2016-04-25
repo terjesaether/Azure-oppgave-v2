405ec008ad9ba2fa05923c449ce44437a4125e1e
terje <terje@MBP-WIN>
2016-04-25 18:50:46 +0200

    Add project files.

---
+++ Azure-oppgave-v2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Azure_oppgave_v2.MvcApplication" Language="C#" %>
---
+++ Azure-oppgave-v2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Azure_oppgave_v2.Startup))]
+namespace Azure_oppgave_v2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
